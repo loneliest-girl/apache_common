af6854083817bd149cee83e5441291167b0a8232
zjx <249910111@qq.com>
2021-07-05 21:50:24 +0800

    初始化项目

---
+++ src/main/java/com/lgirl/apachecommon/ApachecommonApplication.java
@@ -0,0 +1,13 @@
+package com.lgirl.apachecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachecommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApachecommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgirl/apachecommon/ApachecommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgirl.apachecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachecommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
